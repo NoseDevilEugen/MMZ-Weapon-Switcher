9bfe827edebc907996f5a2967d6e47a97ca86fdc
Яцкин Евгений <yatskin.evgeny@csoft.ru>
2025-11-26 12:08:21 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -18,11 +18,64 @@ enum ZXForms
     eOX
 };
 
+enum ZeroElement
+{
+    eNone,
+    eElec,
+    eFire,
+    eIce,
+};
+
+enum ZeroWeapon
+{
+    eBuster,
+    eSaber,
+    eRod,
+    eShield,
+};
+
+
+//set flags for unlocked chips \ weapons for MMZ games
+void SetFlags(std::map<uint8_t, bool>& arrFlags, uint8_t* pAddress)
+{
+    arrFlags[eBuster] = *pAddress >> eBuster & 1;//also no-chip
+    arrFlags[eSaber] = *pAddress >> eSaber & 1;//also Elec chip
+    arrFlags[eRod] = *pAddress >> eRod & 1;//also Fire chip
+    arrFlags[eShield] = *pAddress >> eShield & 1;//also Ice chip
+}
+
+//set flags for unlocked bodies (MMZ3, elemental only)
+void SetZ3Flags(std::map<uint8_t, bool>& arrFlags, uint8_t* pAddress)
+{
+    arrFlags[eNone] = *pAddress >> eNone & 1;
+    arrFlags[eElec] = *pAddress >> eElec+2 & 1;
+    arrFlags[eFire] = *pAddress >> eFire+2 & 1;
+    arrFlags[eIce] = *pAddress >> eIce+2 & 1;
+}
+
+//change chip if unlocked (MMZ1\MMZ2\MMZ3)
+void MMZSetChipValue(uint8_t nNewValue, uint8_t& nChipValue, std::map<uint8_t, bool> arrFlags)
+{
+    if (arrFlags[nNewValue] == true)
+    {
+        nChipValue = nNewValue;
+    }
+}
+
+//change body value according to elemental chip value
+uint8_t MMZSetBodyValue(uint8_t& nChipValue)
+{
+    if (nChipValue == 0)
+        return 0;
+    else
+        return nChipValue + 2;
+}
+
+//switch to next available form (MMZX)
 void IncreaseValueZX(uint8_t& nOldValue, std::map<uint8_t, bool> arrFlags, uint8_t& nVSpeed)
 {
     if (nOldValue < 7)
     {
-        nVSpeed = 1;
         nOldValue++;
         while (arrFlags[nOldValue] != true)
         {
@@ -36,7 +89,6 @@ void IncreaseValueZX(uint8_t& nOldValue, std::map<uint8_t, bool> arrFlags, uint8
     }
     else if (nOldValue == 7)
     {
-        nVSpeed = 1;
         nOldValue = 0;
         while (arrFlags[nOldValue] != true)
         {
@@ -50,12 +102,12 @@ void IncreaseValueZX(uint8_t& nOldValue, std::map<uint8_t, bool> arrFlags, uint8
     }
 }
 
+//switch to previous available form (MMZX)
 void DecreaseValueZX(uint8_t& nOldValue, std::map<uint8_t, bool> arrFlags, uint8_t& nVSpeed)
 {
 
     if (nOldValue > 0)
     {
-        nVSpeed = 1;
         nOldValue--;
         while (arrFlags[nOldValue] != true)
         {
@@ -69,7 +121,6 @@ void DecreaseValueZX(uint8_t& nOldValue, std::map<uint8_t, bool> arrFlags, uint8
     }
     else if (nOldValue == 0)
     {
-        nVSpeed = 1;
         nOldValue = 7;
         while (arrFlags[nOldValue] != true)
         {
@@ -83,6 +134,7 @@ void DecreaseValueZX(uint8_t& nOldValue, std::map<uint8_t, bool> arrFlags, uint8
     }
 }
 
+//next subweapon in MMZ4 (does not work if weapon stolen)
 void IncreaseValueZ4(uint8_t& nOldValue, uint8_t& nMainValue)
 {
     if (nOldValue < 3)
@@ -103,6 +155,7 @@ void IncreaseValueZ4(uint8_t& nOldValue, uint8_t& nMainValue)
     }
 }
 
+//previous subweapon in MMZ4 (does not work if weapon stolen)
 void DecreaseValueZ4(uint8_t& nOldValue, uint8_t& nMainValue)
 {
     if (nOldValue < 3)
@@ -122,37 +175,57 @@ void DecreaseValueZ4(uint8_t& nOldValue, uint8_t& nMainValue)
     }
 }
 
-void IncreaseValue(uint8_t& nOldValue, uint8_t& nMainValue)
+//next available subweapon in MMZ1\MMZ2\MMZ3
+void IncreaseValue(uint8_t& nOldValue, uint8_t& nMainValue, std::map<uint8_t, bool> arrFlags)
 {
+    if (nOldValue == nMainValue)
+        return;
 
     if (nOldValue == 3)
-        nOldValue = 0;
-    else
-        nOldValue++;
-
-    if (nOldValue == nMainValue)
     {
-        if (nOldValue >= 3)
-            nOldValue = 0;
-        else
+        nOldValue = 0;
+        while (arrFlags[nOldValue] != true || nOldValue == nMainValue)
+        {
             nOldValue++;
+        }
     }
+    else
+    {
+        nOldValue++;
+        while (arrFlags[nOldValue] != true || nOldValue == nMainValue)
+        {
+            nOldValue++;
+            if (nOldValue > 3)
+                nOldValue = 0;
+        }
 
+    }
 }
 
-void DecreaseValue(uint8_t& nOldValue, uint8_t& nMainValue)
+//previous available subweapon in MMZ1\MMZ2\MMZ3
+void DecreaseValue(uint8_t& nOldValue, uint8_t& nMainValue, std::map<uint8_t, bool> arrFlags)
 {
-    if (nOldValue == 0)
-        nOldValue = 3;
-    else
-        nOldValue--;
-   
     if (nOldValue == nMainValue)
+        return;
+
+    if (nOldValue == 0)
     {
-        if (nOldValue <= 0)
-            nOldValue = 3;
-        else
+        nOldValue = 3;
+        while (arrFlags[nOldValue] != true || nOldValue == nMainValue)
+        {
             nOldValue--;
+        }
+    }
+    else
+    {
+        nOldValue--;
+        while (arrFlags[nOldValue] != true || nOldValue == nMainValue)
+        {
+            nOldValue--;
+            if (nOldValue < 0)
+                nOldValue = 3;
+        }
+
     }
 }
 
@@ -163,9 +236,10 @@ void MyThreadFunction()
     if (exeBase == NULL) {
         return;
     }
-    uint8_t* exeBasePtr = (uint8_t*)exeBase;   
 
+    uint8_t* exeBasePtr = (uint8_t*)exeBase;   
     
+    //MMZ1
     uint8_t* pMMZ1Sub = exeBasePtr + 0x252236B;
     uint8_t* pMMZ1Chip = exeBasePtr + 0x252237D;
     uint8_t* pMMZ1Main = exeBasePtr + 0x252236A;
@@ -174,6 +248,13 @@ void MyThreadFunction()
     uint8_t& nMMZ1ChipValue = *pMMZ1Chip;
     uint8_t& nMMZ1MainValue = *pMMZ1Main;
 
+    uint8_t* pMMZ1Weapons = exeBasePtr + 0x2522368;
+    std::map<uint8_t, bool> arrMMZ1Weapons;
+
+    uint8_t* pMMZ1Chips = exeBasePtr + 0x2522369;
+    std::map<uint8_t, bool> arrMMZ1Chips;
+
+    //MMZ2
     uint8_t* pMMZ2Sub = exeBasePtr + 0x2529CA1;
     uint8_t* pMMZ2Chip = exeBasePtr + 0x2529CA3;
     uint8_t* pMMZ2Main = exeBasePtr + 0x2529CA0;
@@ -182,22 +263,36 @@ void MyThreadFunction()
     uint8_t& nMMZ2ChipValue = *pMMZ2Chip;
     uint8_t& nMMZ2MainValue = *pMMZ2Main;
 
+    uint8_t* pMMZ2Weapons = exeBasePtr + 0x2529C9E;
+    std::map<uint8_t, bool> arrMMZ2Weapons;
+
+    uint8_t* pMMZ2Chips = exeBasePtr + 0x2529C9F;
+    std::map<uint8_t, bool> arrMMZ2Chips;
+
+    //MMZ3
     uint8_t* pMMZ3Sub = exeBasePtr + 0x2535A45;
     uint8_t* pMMZ3Chip = exeBasePtr + 0x2535A46;
     uint8_t* pMMZ3Body = exeBasePtr + 0x2535A48;
     uint8_t* pMMZ3Main = exeBasePtr + 0x2535A44;
+
+    uint8_t* pMMZ3Weapons = exeBasePtr + 0x2535A4E;
+    std::map<uint8_t, bool> arrMMZ3Weapons;
+
+    uint8_t* pMMZ3Chips = exeBasePtr + 0x2535A50;
+    std::map<uint8_t, bool> arrMMZ3Chips;
 
     uint8_t& nMMZ3SubValue = *pMMZ3Sub;
     uint8_t& nMMZ3ChipValue = *pMMZ3Chip;
     uint8_t& nMMZ3BodyValue = *pMMZ3Body;
     uint8_t& nMMZ3MainValue = *pMMZ3Main;
 
+    //MMZX
     uint8_t* pMMZXBody = exeBasePtr + 0x28B5E70;
     
     uint8_t& nMMZXBodyValue = *pMMZXBody;
 
     std::map<uint8_t, bool> arrFlags;
-    arrFlags[eHuman] = true;
+    arrFlags[eHuman] = true;//always true
 
     uint8_t* pMMZXBodyX = exeBasePtr + 0x28B13A3;
 
@@ -208,8 +303,7 @@ void MyThreadFunction()
     uint8_t* pMMZXReset = exeBasePtr + 0x28B5C99;
     uint8_t& pMMZXResetValue = *pMMZXReset;
 
-   
-    
+    //MMZ4
     uint8_t* pMMZ4Sub = exeBasePtr + 0x2541235;
     uint8_t* pMMZ4Knuckle = exeBasePtr + 0x2541237;
     uint8_t* pMMZ4Toss = exeBasePtr + 0x254126E;
@@ -226,6 +320,19 @@ void MyThreadFunction()
     bool rtPressed = false;
     while (x)
     {
+        //mmz1 checks for unlocked weapons and chips
+        SetFlags(arrMMZ1Weapons, pMMZ1Weapons);
+        SetFlags(arrMMZ1Chips, pMMZ1Chips);
+
+        //mmz2 checks for unlocked weapons and chips
+        SetFlags(arrMMZ2Weapons, pMMZ2Weapons);
+        SetFlags(arrMMZ2Chips, pMMZ2Chips);
+
+        //mmz3 checks for unlocked weapons and chips
+        SetFlags(arrMMZ3Weapons, pMMZ3Weapons);
+        SetZ3Flags(arrMMZ3Chips, pMMZ3Chips);
+
+        //MMZX checks for unlocked forms
         arrFlags[eX] = *pMMZXBodyX >>7 & 1;
         arrFlags[eZX] = *pMMZXBodyZX >> 0 & 1;
         arrFlags[eHX] = *pMMZXBodyZX >> 1 & 1;
@@ -239,7 +346,7 @@ void MyThreadFunction()
         ZeroMemory(&state, sizeof(XINPUT_STATE));
         if (XInputGetState(0, &state) == ERROR_SUCCESS)
         {
-            //subweapon switch goes brrrrr
+            //previous subweapon / form
             if (state.Gamepad.bLeftTrigger < TRIGGER_THRESHOLD)
             {
                 ltPressed = false;
@@ -249,14 +356,14 @@ void MyThreadFunction()
 
             {
                 ltPressed = true;
-                DecreaseValue(nMMZ1SubValue, nMMZ1MainValue);
-                DecreaseValue(nMMZ2SubValue, nMMZ2MainValue);
-                DecreaseValue(nMMZ3SubValue, nMMZ3MainValue);
+                DecreaseValue(nMMZ1SubValue, nMMZ1MainValue, arrMMZ1Weapons);
+                DecreaseValue(nMMZ2SubValue, nMMZ2MainValue, arrMMZ2Weapons);
+                DecreaseValue(nMMZ3SubValue, nMMZ3MainValue, arrMMZ3Weapons);
                 DecreaseValueZX(nMMZXBodyValue, arrFlags, pMMZXResetValue);
                 DecreaseValueZ4(nMMZ4SubValue, nMMZ4MainValue);
-                //nMMZ4Knuckle = 0;
-                //nMMZ4Toss = 2;
             }
+
+            //next subweapon / form
             if (state.Gamepad.bRightTrigger < TRIGGER_THRESHOLD)
             {
                 rtPressed = false;
@@ -265,50 +372,52 @@ void MyThreadFunction()
                 && rtPressed == false)
             {
                 rtPressed = true;
-                IncreaseValue(nMMZ1SubValue, nMMZ1MainValue);
-                IncreaseValue(nMMZ2SubValue, nMMZ2MainValue);
-                IncreaseValue(nMMZ3SubValue, nMMZ3MainValue);
+                IncreaseValue(nMMZ1SubValue, nMMZ1MainValue, arrMMZ1Weapons);
+                IncreaseValue(nMMZ2SubValue, nMMZ2MainValue, arrMMZ2Weapons);
+                IncreaseValue(nMMZ3SubValue, nMMZ3MainValue, arrMMZ3Weapons);
                 IncreaseValueZX(nMMZXBodyValue, arrFlags, pMMZXResetValue);
                 IncreaseValueZ4(nMMZ4SubValue, nMMZ4MainValue);
-                //nMMZ4Knuckle = 0;
-                //nMMZ4Toss = 2;
-            }
-            //element switch goes brrrrrr
-            if (state.Gamepad.sThumbRY > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
-            {
-                nMMZ1ChipValue = 0;
-                nMMZ2ChipValue = 0;
-                nMMZ3ChipValue = 0;
-                nMMZ3BodyValue = 0;
-            }
-            if (state.Gamepad.sThumbRY < -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
-            {
-                nMMZ1ChipValue = 2;
-                nMMZ2ChipValue = 2;
-                nMMZ3ChipValue = 2;
-                nMMZ3BodyValue = 4;
             }
 
+            //set element to None (right stick up)
+            if (state.Gamepad.sThumbRY > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
+            {
+                MMZSetChipValue(eNone, nMMZ1ChipValue, arrMMZ1Chips);
+                MMZSetChipValue(eNone, nMMZ2ChipValue, arrMMZ2Chips);
+                MMZSetChipValue(eNone, nMMZ3ChipValue, arrMMZ3Chips);
+                nMMZ3BodyValue = MMZSetBodyValue(nMMZ3ChipValue);
+            }
+
+            //set element to Fire (right stick down)
+            if (state.Gamepad.sThumbRY < -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
+            {
+                MMZSetChipValue(eFire, nMMZ1ChipValue, arrMMZ1Chips);
+                MMZSetChipValue(eFire, nMMZ2ChipValue, arrMMZ2Chips);
+                MMZSetChipValue(eFire, nMMZ3ChipValue, arrMMZ3Chips);
+                nMMZ3BodyValue = MMZSetBodyValue(nMMZ3ChipValue);
+            }
+
+            //set element to Elec (right stick right)
             if (state.Gamepad.sThumbRX > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
             {
-                nMMZ1ChipValue = 1;
-                nMMZ2ChipValue = 1;
-                nMMZ3ChipValue = 1;
-                nMMZ3BodyValue = 3;
+                MMZSetChipValue(eElec, nMMZ1ChipValue, arrMMZ1Chips);
+                MMZSetChipValue(eElec, nMMZ2ChipValue, arrMMZ2Chips);
+                MMZSetChipValue(eElec, nMMZ3ChipValue, arrMMZ3Chips);
+                nMMZ3BodyValue = MMZSetBodyValue(nMMZ3ChipValue);
             }
+
+            //set element to Ice (right stick left)
             if (state.Gamepad.sThumbRX < -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
             {
-                nMMZ1ChipValue = 3;
-                nMMZ2ChipValue = 3;
-                nMMZ3ChipValue = 3;
-                nMMZ3BodyValue = 5;
+                MMZSetChipValue(eIce, nMMZ1ChipValue, arrMMZ1Chips);
+                MMZSetChipValue(eIce, nMMZ2ChipValue, arrMMZ2Chips);
+                MMZSetChipValue(eIce, nMMZ3ChipValue, arrMMZ3Chips);
+                nMMZ3BodyValue = MMZSetBodyValue(nMMZ3ChipValue);
             }
         }
     }
     MessageBox(NULL, L"Thread terminated", NULL, MB_ICONEXCLAMATION);
 }
-
-
 
 #define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
 
